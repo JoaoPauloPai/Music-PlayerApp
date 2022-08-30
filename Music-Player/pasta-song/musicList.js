50222f27466471394f6d0162932ba59e4d8eca00
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-08-29 22:23:31 -0300

    atualização de pasta
    atualização

---
+++ Music-Player/pasta-song/musicList.js
@@ -0,0 +1,7 @@
+//para adicionar mais músicas, basta copiar o código a seguir e colar dentro do array. 
+
+// {
+//      name: "Aqui nome da musica" - "Here is the music name", 
+//      artist: "Aqui nome do artista" - "Here is the artist name",
+//      img: "imagem aqui" - (relembre formato deve estar em fomato .jpg.)      
+//      src: "musica aqui" - (relembre formato deve estar em .mp3 )
