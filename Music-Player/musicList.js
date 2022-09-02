adb2059aa55d28d19e5486180c194b63a5395e94
KatiaWinkert <89809681+KatiaWinkert@users.noreply.github.com>
2022-09-01 22:40:10 -0300

    musicList.js
    implementação

--- Music-Player/musicList.js
+++ Music-Player/musicList.js
@@ -1,7 +1,40 @@
-//para adicionar mais músicas, basta copiar o código a seguir e colar dentro do array. 
+//para adicionar mais músicas, basta copiar o código a seguir e colar dentro do array.
 
 // {
-//      name: "Aqui nome da musica" - "Here is the music name", 
+//      name: "Aqui nome da musica" - "Here is the music name",
 //      artist: "Aqui nome do artista" - "Here is the artist name",
-//      img: "imagem aqui" - (relembre formato deve estar em fomato .jpg.)      
-//      src: "musica aqui" - (relembre formato deve estar em .mp3 )
+//      img: "nome imagem aqui" - (relembre formato deve estar em fomato .jpg.)
+//      src: "nome musica aqui" - (relembre formato deve estar em .mp3 )
+
+let allMusic = [
+  {
+    name: 'Under the influence', // nome da musica
+    artist: 'Chris Brown', // nome do artista
+    img: 'music-1', // image nome
+    src: 'music-1', // nome aqui
+  },
+  {
+    name: 'Free of the Dark',
+    artist: 'Iron Maiden',
+    img: 'music-2',
+    src: 'music-2',
+  },
+  {
+    name: 'Paro',
+    artist: 'Nej',
+    img: 'music-3',
+    src: 'music-3',
+  },
+  {
+    name: 'Shut up lisen',
+    artist: 'Nicholas Bonnie',
+    img: 'music-4',
+    src: 'music-4',
+  },
+  {
+    name: 'System Of Down',
+    artist: 'System Of Down',
+    img: 'music-5',
+    src: 'music-5',
+  },
+]
