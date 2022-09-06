6fcd189112894c6642bd03a106c4e879bca992ea
Joao paulo Gomes da Silva <joaopaulopai99@gmail.com>
2022-09-06 17:17:03 -0400

    COMMIT 06/09/22 CRIANDO FUNCTION COUNT

--- assets/index.js
+++ assets/index.js
@@ -12,6 +12,8 @@ const wrapper = document.querySelector('.wrapper'),
 let musicIndex = Math.floor(Math.random() * allMusic.lenght + 1)
 isMusicPaused = true
 
+let contMusic = 0;
+
 window.addEventListener('load', () => {
   loadMusic(musicIndex)
 })
@@ -20,19 +22,49 @@ function loadMusic(indexNumb) {
   musicName.inneText = allMusic[indexNumb - 1].name
   musicArtist.inneText = allMusic[indexNumb - 1].artist
   musicImg.src = `assets/image${allMusic[indexNumb - 1].src}.jpg`
-  mainAudio.src = `assets/songs${allMusic[indexNumb - 1].src}.mp3`
+  /* mainAudio.src = `assets/songs${allMusic[indexNumb - 1].src}.mp3`*/
+}
+function playMusic() {
+  contMusic = 1;
+
+  //     wrapper.classList.add('paused')
+      musicImg.classList.add('rotate')
+  // playPauseBtn.innerHTML 
+
+
+  /* wrapper.classList.add('paused')
+   musicImg.classList.add('rotate')
+   playPauseBtn.innerHTML */
+  return contMusic();
 }
 
-function playMusic() {
+function playMusic1() {
   wrapper.classList.add('paused')
   musicImg.classList.add('rotate')
-  playPauseBtn.innerHTML = `<i class="fi fi-sr-pause"></i>`
-  mainAudio.play()
+  playPauseBtn.innerHTML
 }
 
 function pauseMusic() {
-  wrapper.classList.remove('paused')
-  musicImg.classList.remove('rotate')
-  playPauseBtn.innerHTML = `<i class="fi fi-sr-play"></i>`
-  mainAudio.pause()
+  contMusic = 0;
+ // musicImg.classList.reload();
+  window.location.reload();
+ // wrapper.classList.remove('paused')
+ // musicImg.classList.remove('rotate')
+  // playPauseBtn.innerHTML = `<i class="fi fi-sr-play"></i>`
+  // mainAudio.pause()
+
+  return contMusic();
 }
+
+function fContMusic() {
+  if (contMusic == 1) {
+    pauseMusic();
+  }
+  if (contMusic == 0) {
+
+    playMusic();
+  }
+
+}
+
+
--- assets/music-list.js
+++ assets/music-list.js
@@ -1,6 +1,6 @@
 //para adicionar mais músicas, basta copiar o código a seguir e colar dentro do array.
 
-// {
+
 //      name: "Aqui nome da musica" - "Here is the music name",
 //      artist: "Aqui nome do artista" - "Here is the artist name",
 //      img: "nome imagem aqui" - (relembre formato deve estar em fomato .jpg.)
